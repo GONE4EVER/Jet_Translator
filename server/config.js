96603c20e850796b6d0319c01dfdc1c229e93487
Nikita <nromanchuk@xbsoftware.by>
2018-02-01 15:11:11 +0300

    start

---
+++ server/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	SERVER_PATH: "./server/"
+};
---
+++ sources/views/data.js
@@ -0,0 +1,10 @@
+import {JetView} from "webix-jet";
+
+export default class DataView extends JetView{
+	config(){
+		return { view:"datatable", autoConfig:true };
+	}
+	init(view){
+		view.parse(data);
+	}
+}
